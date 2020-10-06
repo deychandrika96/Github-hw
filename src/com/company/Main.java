caf1064442630e7cdd1261f0d8c7c4d1d6464b7e
Chandrika <sejutispr.dey01@gmail.com>
2020-10-05 20:46:32 -0400

    Homework for github

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-         System.out.println("chandrikadey");
+
+       //I am creating a while loop to find the numbers between 23 and 99
+        int x = 23;
+        while(x < 99) {
+
+            System.out.println("Value of x :" + x);
+            x++;
+        }
+
     }
 }
